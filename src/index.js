03677fe2315c557ea8b288425dce56a286427393
Alessandro Contenti <a.contenti@gmail.com>
2019-06-11 22:38:58 +0200

    change formatting + bug fixes

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@
 				return yamldoc2md(yamldoc);
 			}
 		};
-	let g;
+	let g = window;
 	if (typeof window !== "undefined") {
 		g = window
 	} else if (typeof global !== "undefined") {
@@ -34,13 +34,15 @@
 	function objectMap(object, mapFn) {
 		return object != null ? Object.keys(object).map(function (key) {
 			return mapFn(key, object[key]);
-		}) : null;
+		}) : [''];
 	}
 
 	function styleHtml(node) {
+		if (typeof node !== 'string') return node;
 		return node
 			.replace(/</g, "&lt;")
 			.replace(/>/g, "&gt;")
+			.replace(/\n/g, "<br>")
 			.replace(/(^|[^\\])__([^]*?[^\\])__/g, "$1<strong>$2</strong>")
 			.replace(/(^|[^\\])_([^]*?[^\\_])_/g, "$1<em>$2</em>")
 			.replace(/(^|[^\\])~~([^~]*[^\\])~~/g, "$1<del>$2</del>")
@@ -48,29 +50,36 @@
 	}
 
 	function styleMd(node) {
+		if (typeof node !== 'string') return node;
 		return node.replace(/(^|[^\\~])~([^~]*[^\\~])~/g, "$1$2");
 	}
 
-	function parseNodeHtml(node, indent) {
-		let spaces = '\t'.repeat(indent);
+	function parseNodeHtml(node, indent, headingLevel) {
+		const spaces = '\t'.repeat(indent);
 		if (Array.isArray(node)) return spaces + "<ul>\n" + node.map(function (it) {
-			return spaces + "\t<li>\n" + parseNodeHtml(it, indent + 2) + spaces + "\t</li>\n";
+			return spaces + "\t<li>\n"
+				+ parseNodeHtml(it, indent + 1, headingLevel)
+				+ spaces + "\t</li>\n";
 		}).join("") + spaces + "</ul>\n";
 		else if (typeof node === 'object') {
-			return spaces + "<ul>\n" + objectMap(node, function (key, it) {
-				return spaces + "\t<li>\n" + spaces + "\t\t" + styleHtml(key) + ":\n" + parseNodeHtml(it, indent + 2) + spaces + "\t</li>\n";
-			}).join("") + spaces + "</ul>\n";
-		} else return spaces + styleHtml(node) + "\n";
+			return spaces + "\t<ul>\n" + objectMap(node, function (key, it) {
+				const headingSize = headingLevel <= 6 ? headingLevel : 6;
+				return spaces + "\t\t<h" + headingSize + ">" + styleHtml(key) + "</h" + headingSize + ">\n"
+					+ parseNodeHtml(it, indent + 1, headingLevel + 1);
+			}).join("") + spaces + "\t</ul>\n";
+		} else return spaces + "\t<ul>\n" + spaces + "\t\t" + styleHtml(node) + "\n" + spaces + "\t</ul>\n";
 	}
 
-	function parseNodeMd(node, indent) {
-		let spaces = ' '.repeat((indent + 1) * 2);
+	function parseNodeMd(node, indent, headingLevel) {
+		let spaces = ' '.repeat(indent * 2);
 		if (Array.isArray(node)) return node.map(function (it) {
-			return "\n" + spaces + "* " + parseNodeMd(it, indent + 1);
+			return spaces + "  * " + parseNodeMd(it, indent + 1, headingLevel);
 		}).join("");
 		else if (typeof node === 'object') {
 			return objectMap(node, function (key, it) {
-				return "\n" + spaces + "* " + styleMd(key) + ": " + parseNodeMd(it, indent + 1);
+				const headingSize = headingLevel <= 6 ? headingLevel : 6;
+				return "#".repeat(headingSize) + " " + styleMd(key) + "\n"
+					+ parseNodeMd(it, indent, headingLevel + 1);
 			}).join("");
 		} else return styleMd(node) + "\n";
 	}
@@ -86,7 +95,7 @@
 		}
 		if (typeof doc === 'string') return "<div>\n\t" + styleHtml(doc) + "\n</div>";
 		else return "<div>\n" + objectMap(doc, function (key, it) {
-			return "\t<h1>" + key + "</h1>\n" + parseNodeHtml(it, 1);
+			return "\t<h1>" + key + "</h1>\n" + parseNodeHtml(it, 1, 2);
 		}).join("") + '</div>';
 	}
 
@@ -101,7 +110,7 @@
 		}
 		if (typeof doc === 'string') return styleMd(doc);
 		else return objectMap(doc, function (key, it) {
-			return "# " + key + "\n" + parseNodeMd(it, 0);
+			return "# " + key + "\n" + parseNodeMd(it, 0, 2);
 		}).join("\n\n")
 	}
 })();
