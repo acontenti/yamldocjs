56b68887bcc8e957d166b1bd8034b84acc5c1f4b
Alessandro Contenti <a.contenti@gmail.com>
2019-06-12 11:20:18 +0200

    many bug fixes & improvements + add other markdown-inspired styling

--- src/index.js
+++ src/index.js
@@ -46,28 +46,38 @@
 			.replace(/(^|[^\\])__([^]*?[^\\])__/g, "$1<strong>$2</strong>")
 			.replace(/(^|[^\\])_([^]*?[^\\_])_/g, "$1<em>$2</em>")
 			.replace(/(^|[^\\])~~([^~]*[^\\])~~/g, "$1<del>$2</del>")
-			.replace(/(^|[^\\])~([^~]*[^\\])~/g, "$1<u>$2</u>");
+			.replace(/(^|[^\\])~([^~]*[^\\])~/g, "$1<sub>$2</sub>")
+			.replace(/(^|[^\\])\^([^^]*[^\\])\^/g, "$1<sup>$2</sup>")
+			.replace(/(^|[^\\])\+\+([^+]*[^\\])\+\+/g, "$1<ins>$2</ins>")
+			.replace(/(^|[^\\])==([^=]*[^\\])==/g, "$1<mark>$2</mark>")
+			.replace(/(^|[^\\])```([^`]*[^\\])```/g, "$1<pre><code>$2</code></pre>");
 	}
 
 	function styleMd(node) {
-		if (typeof node !== 'string') return node;
-		return node.replace(/(^|[^\\~])~([^~]*[^\\~])~/g, "$1$2");
+		return node;
 	}
 
-	function parseNodeHtml(node, indent, headingLevel) {
+	function parseNodeHtml(node, indent, headingLevel, slideContent) {
 		const spaces = '\t'.repeat(indent);
 		if (Array.isArray(node)) return spaces + "<ul>\n" + node.map(function (it) {
 			return spaces + "\t<li>\n"
-				+ parseNodeHtml(it, indent + 1, headingLevel)
+				+ parseNodeHtml(it, indent + 2, headingLevel, false)
 				+ spaces + "\t</li>\n";
 		}).join("") + spaces + "</ul>\n";
 		else if (typeof node === 'object') {
-			return spaces + "\t<ul>\n" + objectMap(node, function (key, it) {
-				const headingSize = headingLevel <= 6 ? headingLevel : 6;
-				return spaces + "\t\t<h" + headingSize + ">" + styleHtml(key) + "</h" + headingSize + ">\n"
-					+ parseNodeHtml(it, indent + 1, headingLevel + 1);
-			}).join("") + spaces + "\t</ul>\n";
-		} else return spaces + "\t<ul>\n" + spaces + "\t\t" + styleHtml(node) + "\n" + spaces + "\t</ul>\n";
+			return (slideContent ? spaces + "<ul>\n" : "")
+				+ objectMap(node, function (key, it) {
+					const headingSize = headingLevel <= 6 ? headingLevel : 6;
+					return (slideContent ? spaces + "\t" : spaces)
+						+ "<h" + headingSize + ">" + styleHtml(key) + "</h" + headingSize + ">\n"
+						+ parseNodeHtml(it, indent + (slideContent ? 1 : 0), headingLevel + 1, true);
+				}).join("")
+				+ (slideContent ? spaces + "</ul>\n" : "");
+		} else {
+			return (slideContent ? spaces + "<ul>\n" + spaces + "\t" : spaces)
+				+ "<span>" + styleHtml(node) + "</span>"
+				+ (slideContent ? "\n" + spaces + "</ul>\n" : "\n");
+		}
 	}
 
 	function parseNodeMd(node, indent, headingLevel) {
@@ -93,10 +103,15 @@
 			console.log(e);
 			return undefined;
 		}
-		if (typeof doc === 'string') return "<div>\n\t" + styleHtml(doc) + "\n</div>";
-		else return "<div>\n" + objectMap(doc, function (key, it) {
-			return "\t<h1>" + key + "</h1>\n" + parseNodeHtml(it, 1, 2);
-		}).join("") + '</div>';
+		if (Array.isArray(doc)) {
+			return parseNodeHtml(doc, 0, 1, false);
+		} else if (typeof doc === 'string') {
+			return styleHtml(doc);
+		} else if (typeof doc === 'object') {
+			return objectMap(doc, function (key, it) {
+				return "<h1>" + key + "</h1>\n" + parseNodeHtml(it, 0, 2, true);
+			}).join("");
+		} else return doc;
 	}
 
 	function yamldoc2md(yamldoc) {
@@ -108,9 +123,14 @@
 			console.log(e);
 			return undefined;
 		}
-		if (typeof doc === 'string') return styleMd(doc);
-		else return objectMap(doc, function (key, it) {
-			return "# " + key + "\n" + parseNodeMd(it, 0, 2);
-		}).join("\n\n")
+		if (Array.isArray(doc)) {
+			return parseNodeMd(doc, 0, 1);
+		} else if (typeof doc === 'string') {
+			return styleMd(doc);
+		} else if (typeof doc === 'object') {
+			return objectMap(doc, function (key, it) {
+				return "# " + key + "\n" + parseNodeMd(it, 0, 2);
+			}).join("\n\n")
+		} else return doc;
 	}
 })();
